d2d6a9566b9aac9ad8df2bebe8dfb7f21b06085a
Ly Bao Khanh <baokhanh7m@hotmail.com>
2016-09-03 07:47:14 +0700

    [ADD] api controller

---
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheWorld.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripController : Controller
+    {
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            if (true) return BadRequest("Bad things happened");
+
+            return Ok(new Trip() { Name = "My Trip" });
+        }
+    }
+}
