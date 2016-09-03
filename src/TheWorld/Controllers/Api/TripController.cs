ca8b749035a385e66d50afc1bbf45fcdf9d419ca
Ly Bao Khanh <baokhanh7m@hotmail.com>
2016-09-03 08:42:32 +0700

    [ADD] using POST

--- src/TheWorld/Controllers/Api/TripController.cs
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -9,6 +9,7 @@ using TheWorld.Models;
 
 namespace TheWorld.Controllers.Api
 {
+    [Route("api/trips")]
     public class TripController : Controller
     {
         private IWorldRepository _repository;
@@ -18,12 +19,18 @@ namespace TheWorld.Controllers.Api
             _repository = repository;
         }
 
-        [HttpGet("api/trips")]
+        [HttpGet("")]
         public IActionResult Get()
         {
             //if (true) return BadRequest("Bad things happened");
 
             return Ok(_repository.GetAllTrips());
+        }
+
+        [HttpPost("")]
+        public IActionResult Post([FromBody] Trip theTrip)
+        {
+            return Ok(true);
         }
     }
 }
