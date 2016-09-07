b36e3171dd7dbddc8270385c78723a07e1f37bb2
Ly Bao Khanh <baokhanh7m@hotmail.com>
2016-09-07 21:39:53 +0700

    [ADD] routing

--- src/TheWorld/wwwroot/js/app-trips.js
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -2,5 +2,15 @@
     'use strict';
 
     // creating the module
-    angular.module('app-trips', ['simpleControls']);
+    angular.module('app-trips', ['simpleControls', 'ngRoute'])
+        .config(function ($routeProvider) {
+
+            $routeProvider.when("/", {
+                controller: "tripController",
+                controllerAs: "vm",
+                templateurl: "/views/tripsView.html"
+            });
+
+            $routeProvider.otherwise({ redirectTo: "/" });
+        })
 })();
