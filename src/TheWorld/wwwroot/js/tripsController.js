103a1d1404d795dd42ebe7b01804e4ae39214b97
Ly Bao Khanh <baokhanh7m@hotmail.com>
2016-09-07 20:38:39 +0700

    [ADD] retrieving data with angularjs

--- src/TheWorld/wwwroot/js/tripsController.js
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -6,24 +6,33 @@
         .module('app-trips')
         .controller('tripsController', tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         /* jshint validthis:true */
         var vm = this;
-        vm.trips = [
-            {
-                name: "US Trip",
-                created: new Date()
-            },
-            {
-                name: "World Trip",
-                created: new Date()
-            }];
+        vm.trips = [];
 
         vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                // success
+                angular.copy(response.data, vm.trips);
+            }, function (error) {
+                // failure
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+        });
 
         vm.addTrip = function () {
             vm.trips.push({ name: vm.newTrip.name, created: new Date() });
             vm.newTrip = {};
         };
+
+
     }
 })();
