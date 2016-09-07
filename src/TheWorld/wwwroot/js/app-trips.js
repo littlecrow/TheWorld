a132137b1339afea7e71cf0121d281c126761f4b
Ly Bao Khanh <baokhanh7m@hotmail.com>
2016-09-07 14:27:22 +0700

    [ADD] my first controller

---
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -0,0 +1,6 @@
+﻿(function () {
+    'use strict';
+
+    // creating the module
+    angular.module('app-trips', []);
+})();
---
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    // getting the existing module
+    angular
+        .module('app-trips')
+        .controller('tripsController', tripsController);
+
+    function tripsController() {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.name = 'Little Crow';
+    }
+})();
