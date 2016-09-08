e713b6350c7f7a79af34d065981d3598195b9a7b
Ly Bao Khanh <baokhanh7m@hotmail.com>
2016-09-08 07:33:31 +0700

    [ADD] second views

---
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app-trips')
+        .controller('tripEditorController', tripEditorController);
+
+    function tripEditorController() {
+        var vm = this;
+        vm.name = "Little Crow";
+    }
+
+})();
