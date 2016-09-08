690be3d506594a598e90801582331714e3741c7b
Ly Bao Khanh <baokhanh7m@hotmail.com>
2016-09-08 07:46:07 +0700

    [ADD] calling the API

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -5,13 +5,25 @@
         .module('app-trips')
         .controller('tripEditorController', tripEditorController);
 
-    function tripEditorController($routeParams) {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
 
         vm.tripName = $routeParams.tripName;
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+            .then(function (response) {
+                // success
+                angular.copy(response.data, vm.stops);
+            }, function (err) {
+                // failure
+                vm.errorMessage = "Failed to load stops";
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
     }
 
 })();
