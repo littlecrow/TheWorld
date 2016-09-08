0376971b7ae96f647ae80501e997a98141419548
Ly Bao Khanh <baokhanh7m@hotmail.com>
2016-09-08 08:41:20 +0700

    [ADD] using non-angularjs plugin

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -17,6 +17,7 @@
             .then(function (response) {
                 // success
                 angular.copy(response.data, vm.stops);
+                _showMap(vm.stops);
             }, function (err) {
                 // failure
                 vm.errorMessage = "Failed to load stops";
@@ -24,6 +25,27 @@
             .finally(function () {
                 vm.isBusy = false;
             });
+    }
+
+    function _showMap(stops) {
+        if (stops && stops.length > 0) {
+            
+            var mapStops = _.map(stops, function (item) {
+                return {
+                    lat: item.latitude,
+                    long: item.longitude,
+                    info: item.name
+                };
+            });
+
+            // show map
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
+            });
+        }
     }
 
 })();
